48ec06c44ca7341cc31a82d3eee158605ab2c11c
Aleksei Korzhenkov <oscypek@ya.ru>
2020-07-02 19:42:02 +0300

    Paired reads (pe and mp) stored in tuple

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zga",
-    version="0.0.4",
+    version="0.0.5dev",
     author="Aleksei Korzhenkov",
     author_email="oscypek@ya.ru",
     description="Prokaryotic genome assembly and annotation pipeline",
